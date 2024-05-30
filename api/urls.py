a25e20dce1ccdc6295af3778526bab860748de74
Yoonge <7884008@qq.com>
2024-05-30 20:28:45 +0800

    feat: ✨ implement user create, update & destroy
    Co-authored-by: Svelte <dev.yoonge@gmail.com>
    Co-authored-by: Yoonge <7884008@qq.com>

--- api/urls.py
+++ api/urls.py
@@ -4,17 +4,27 @@ from api import views
 urlpatterns = [
     path("", views.api_root),
     path("tags/", views.TagViewSet.as_view({ "get": "list" }), name="tag-list"),
-    path("topics/", views.TopicViewSet.as_view({ "get": "list", "post": "create" }), name="topic-list"),
-    re_path(r"^topic/(?P<pk>\d+)/$", views.TopicViewSet.as_view({ "get": "retrieve" }), name="topic-detail"),
-    path("users/", views.UserViewSet.as_view({ "get": "list" }), name="user-list"),
-    path("user/<str:username>/", views.UserViewSet.as_view({ "get": "retrieve" }), name="user-detail"),
-    # path("", views.TopicListView.as_view()),
+    path("topics/", views.TopicViewSet.as_view({
+        "get": "list",
+        "post": "create"
+    }), name="topic-list"),
+    re_path(r"^topic/(?P<pk>\d+)/$", views.TopicViewSet.as_view({
+        "delete": "destroy",
+        "get": "retrieve",
+        "put": "update"
+    }), name="topic-detail"),
+    path("users/", views.UserViewSet.as_view({
+        "get": "list",
+        "post": "create"
+    }), name="user-list"),
+    path("user/<str:username>/", views.UserViewSet.as_view({
+        "get": "retrieve",
+        "put": "update",
+        "delete": "destroy"
+    }), name="user-detail"),
     # path("my-topics/", views.UserTopicListView.as_view()),
     # path("my-favorites/", views.FavoriteTopicListView.as_view()),
     # path("profile/<str:username>/", views.UserTopicListView.as_view()),
     # path("profile/<str:username>/favorites/", views.FavoriteTopicListView.as_view()),
-    # re_path(r"^topic/(?P<_id>\d+)/$", views.TopicDetailView.as_view()),
-    # path("user/", views.UserListView.as_view()),
-    # path("user/<str:username>/", views.UserDetailView.as_view()),
     # path("settings/", views.UserSettingsView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,15 +1,16 @@
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.viewsets import ViewSet
 from api.models import Tag, Topic, User
-from api.serializers import UserSerializer, TagSerializer, TopicReadSerializer, TopicWriteSerializer
+from api.serializers import TagSerializer, TopicReadSerializer, TopicWriteSerializer, \
+    UserReadSerializer, UserWriteSerializer
 from api.utils.pagination import CustomPagination
 
 @api_view(["GET"])
-@permission_classes((IsAuthenticatedOrReadOnly,))
+@permission_classes((IsAuthenticatedOrReadOnly, ))
 def api_root(request, format=None):
     """
     API root.
@@ -40,13 +41,11 @@ def fetch_topics(request, username = None, favor = False):
     page = CustomPagination()
     topics = page.paginate_queryset(topics_all, request)
     ser_topics = TopicReadSerializer(topics, many=True)
-    ser_user = UserSerializer(user)
+    ser_user = UserReadSerializer(user)
     return (page, ser_topics.data, total, ser_user.data)
 
 class TagViewSet(ViewSet):
     """
-    A simple ViewSet for tags.
-
     GET list:
     Return a list of all the tags.
 
@@ -66,8 +65,6 @@ class TagViewSet(ViewSet):
 
 class TopicViewSet(ViewSet):
     """
-    A simple ViewSet for topics.
-
     GET list:
     Return a list of all the topics.
 
@@ -75,7 +72,13 @@ class TopicViewSet(ViewSet):
     Return a topic instance.
 
     POST create:
-    Create a new topic instance.
+    Create a new topic instance and return it.
+
+    PUT update:
+    Update a topic instance and return it.
+
+    DELETE destroy:
+    Destroy a topic instance.
     """
     permission_classes = (IsAuthenticatedOrReadOnly, )
 
@@ -88,7 +91,11 @@ class TopicViewSet(ViewSet):
         return page.get_paginated_response(ser.data, msg="Topics query succeed.", total=total)
 
     def retrieve(self, request, pk=None):
-        topic = Topic.objects.get(pk=pk)
+        try:
+            topic = Topic.objects.get(pk=pk)
+        except Topic.DoesNotExist:
+            return Response({ "code": status.HTTP_404_NOT_FOUND, "msg": "Topic not found." })
+
         ser = TopicReadSerializer(topic)
         return Response({ "code": status.HTTP_200_OK, "data": ser.data, "msg": "Topic query succeed." })
 
@@ -96,21 +103,48 @@ class TopicViewSet(ViewSet):
         tags_str = request.data.pop("tags")
         tags = []
         for tag_str in tags_str:
-            tag, _ = Tag.objects.get_or_create(tag=tag_str)
+            tag, _ = Tag.objects.get_or_create(tag=tag_str.lower())
             tags.append(tag._id)
         request.data["tags"] = tags
 
         ser = TopicWriteSerializer(data=request.data)
-        if ser.is_valid():
-            ser.save()
-            return Response({ "code": status.HTTP_201_CREATED, "data": ser.data, "msg": "Topic create succeed." })
-        else:
+        if not ser.is_valid():
             return Response({ "code": status.HTTP_400_BAD_REQUEST, "error": ser.errors, "msg": "Topic create failed." })
+
+        ser.save()
+        return Response({ "code": status.HTTP_201_CREATED, "data": ser.data, "msg": "Topic create succeed." })
+
+    def update(self, request, pk=None):
+        try:
+            topic = Topic.objects.get(pk=pk)
+        except Topic.DoesNotExist:
+            return Response({ "code": status.HTTP_404_NOT_FOUND, "msg": "Topic not found." })
+
+        tags_str = request.data.pop("tags")
+        tags = []
+        for tag_str in tags_str:
+            tag, _ = Tag.objects.get_or_create(tag=tag_str.lower())
+            tags.append(tag._id)
+        request.data["tags"] = tags
+
+        ser = TopicWriteSerializer(topic, data=request.data)
+        if not ser.is_valid():
+            return Response({ "code": status.HTTP_400_BAD_REQUEST, "error": ser.errors, "msg": "Topic update failed." })
+
+        ser.save()
+        return Response({ "code": status.HTTP_200_OK, "data": ser.data, "msg": "Topic update succeed." })
+
+    def destroy(self, request, pk=None):
+        try:
+            topic = Topic.objects.get(pk=pk)
+        except Topic.DoesNotExist:
+            return Response({ "code": status.HTTP_404_NOT_FOUND, "msg": "Topic not found." })
+
+        topic.delete()
+        return Response({ "code": status.HTTP_204_NO_CONTENT, "msg": "Topic delete succeed." })
 
 class UserViewSet(ViewSet):
     """
-    A simple ViewSet for users.
-
     GET list:
     Return a list of all the users.
 
@@ -118,18 +152,67 @@ class UserViewSet(ViewSet):
     Return a user instance.
 
     POST create:
-    Create a new user instance.
+    Create a new user instance and return it.
+
+    PUT update:
+    Update a user instance and return it.
+
+    DELETE destroy:
+    Destroy a user instance.
     """
+    def get_permissions(self):
+        if self.action == "destroy":
+            permission_classes = [IsAdminUser, ]
+        else:
+            permission_classes = [IsAuthenticated, ]
+        return [permission() for permission in permission_classes]
+
     def list(self, request):
         users_all = User.objects.all().order_by("-create_at")
         total = User.objects.count()
         page = CustomPagination()
         users = page.paginate_queryset(users_all, request)
-        ser = UserSerializer(users, many=True)
+        ser = UserReadSerializer(users, many=True)
         return page.get_paginated_response(ser.data, msg="Users query succeed.", total=total)
 
     def retrieve(self, request, *args, **kwargs):
-        username = kwargs.get("username")
-        user = User.objects.get(username=username)
-        ser = UserSerializer(user)
+        try:
+            username = kwargs.get("username")
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response({ "code": status.HTTP_404_NOT_FOUND, "msg": "User not found." })
+
+        ser = UserReadSerializer(user)
         return Response({ "code": status.HTTP_200_OK, "data": ser.data, "msg": "User info query succeed." })
+
+    def create(self, request):
+        ser = UserWriteSerializer(data=request.data)
+        if not ser.is_valid():
+            return Response({ "code": status.HTTP_400_BAD_REQUEST, "error": ser.errors, "msg": "User create failed." })
+
+        ser.save()
+        return Response({ "code": status.HTTP_201_CREATED, "data": ser.data, "msg": "User create succeed." })
+
+    def update(self, request, *args, **kwargs):
+        try:
+            username = kwargs.get("username")
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response({ "code": status.HTTP_404_NOT_FOUND, "msg": "User not found." })
+
+        ser = UserWriteSerializer(user, data=request.data, partial=True)
+        if not ser.is_valid():
+            return Response({ "code": status.HTTP_400_BAD_REQUEST, "error": ser.errors, "msg": "User update failed." })
+
+        ser.save()
+        return Response({ "code": status.HTTP_200_OK, "data": ser.data, "msg": "User update succeed." })
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            username = kwargs.get("username")
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response({ "code": status.HTTP_404_NOT_FOUND, "msg": "User not found." })
+
+        user.delete()
+        return Response({ "code": status.HTTP_204_NO_CONTENT, "msg": "User delete succeed." })
